9238cc3a9ec6230d3d1ba61e85b27e527a19522c
SrKrish0404 <ramakrishnan.s@flsmidth.com>
2025-01-07 09:36:44 +0000

    update1

--- ProjectFiles/NetSolution/DateAndTimeWidgetLogic.cs
+++ ProjectFiles/NetSolution/DateAndTimeWidgetLogic.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using UAManagedCore;
 using FTOptix.SerialPort;
+using FTOptix.RAEtherNetIP;
+using FTOptix.CommunicationDriver;
 #endregion
 
 public class DateAndTimeWidgetLogic : BaseNetLogic
